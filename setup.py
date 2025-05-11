18e4ecc9b53bfc43fd9407630171d6430529fe68
kangjian2016 <jiankang@umich.edu>
2025-05-11 12:58:59 -0400

    Install package requirement updated

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'torch',
-        'time',
-        'matplotlib',
+        'matplotlib'
     ],
     description='A package implements the fast Bayesian linear regression',
     author='Jian Kang',
